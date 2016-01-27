22960aa05ccd0c7705489c602fefb366863fdcb1
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-01-27 10:13:07 +0100

    Adding redirection and subscription check for the controller class.

--- src/Controller/RefreshStatus.php
+++ src/Controller/RefreshStatus.php
@@ -6,10 +6,29 @@
  */
 
 namespace Drupal\acquia_search_multi_subs\Controller;
+use Drupal\acquia_connector\Subscription;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Controller routines to refresh the subscription status.
  */
 class RefreshStatus {
+  public function refresh() {
+    // Code lifted from acquia_connector.
 
-}
+    $config = \Drupal::config('acquia_connector.settings');
+    // Don't send data if site is blocked or missing components.
+    if ($config->get('spi.blocked') || (is_null($config->get('spi.site_name')) && is_null($config->get('spi.site_machine_name')))) {
+      return;
+    }
+    Subscription::update();
+
+    // Return to the settings page for acquia_connector, or to the destination if it was set.
+    $destination = \Drupal::destination()->getAsArray();
+    if (isset($destination['destination'])) {
+      return new RedirectResponse(\Drupal::url($destination['destination']);
+    }
+    else {
+      return $this->redirect('acquia_connector.settings');
+    }
+  }
