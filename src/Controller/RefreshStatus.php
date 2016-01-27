ce42b0514402707bc7a9a4109a88fcd58d12837a
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-01-27 09:39:24 +0100

    Adding the initial Controller class for refreshing the status.

---
+++ src/Controller/RefreshStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\acquia_search_multi_subs\Controller\RefreshStatus.
+ */
+
+namespace Drupal\acquia_search_multi_subs\Controller;
+
+/**
+ * Controller routines to refresh the subscription status.
+ */
+class RefreshStatus {
+
+}
