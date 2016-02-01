928f5570409caa1328fc5213e9722f7c2542bf7a
Balazs Dianiska <balazs.dianiska@acquia.com>
2016-02-01 16:09:09 +0100

    Use the SearchSubscriber override.

--- src/Plugin/search_api/backend/SearchApiSolrAcquiaMultiSubsBackend.php
+++ src/Plugin/search_api/backend/SearchApiSolrAcquiaMultiSubsBackend.php
@@ -8,7 +8,7 @@ use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\acquia_search\EventSubscriber\SearchSubscriber;
+use Drupal\acquia_search_multi_subs\EventSubscriber\SearchSubscriber;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\acquia_connector\Client;
 use Drupal\acquia_connector\CryptConnector;
